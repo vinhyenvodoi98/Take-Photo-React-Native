bfde2bbf3b008cd35f548ce46191f413622bd2c2
hoang <vinhyenvodoi98@gmail.com>
2019-08-06 00:03:00 +0700

    handling Text Input

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
-      <View>
-        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
-        <View style={{ width: 150, height: 150, backgroundColor: 'steelblue' }} />
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder='Type here to translate!'
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text
+            .split(' ')
+            .map((word) => word && '🍕')
+            .join(' ')}
+        </Text>
       </View>
     );
   }
 }
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
