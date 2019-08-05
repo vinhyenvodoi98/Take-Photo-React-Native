954109057d0fba611bd2612b36edf590834d304d
hoang <vinhyenvodoi98@gmail.com>
2019-08-05 23:54:30 +0700

    height and width

--- App.js
+++ App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30
-  },
-  red: {
-    color: 'red'
-  }
-});
-
-export default class LotsOfStyles extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigBlue}>just bigBlue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
+        <View style={{ width: 150, height: 150, backgroundColor: 'steelblue' }} />
       </View>
     );
   }
 }
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
