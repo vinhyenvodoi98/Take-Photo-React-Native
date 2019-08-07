d976f0fcea70fc03397da598408d35a72a7518c7
hoang <vinhyenvodoi98@gmail.com>
2019-08-07 08:37:02 +0700

    bug : RNCamera

--- App.js
+++ App.js
@@ -1,56 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Home from './view/Home.js';
 
-import Camera from 'react-native-camera';
-
-export default class App extends Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.view}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
-            [CAPTURE_IMAGE]
-          </Text>
-        </Camera>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Home />
       </View>
     );
   }
-
-  takePicture() {
-    const options = {};
-
-    this.camera
-      .capture({ metadata: options })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  view: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: 'steelblue',
-    borderRadius: 10,
-    color: 'red',
-    padding: 15,
-    margin: 45
-  }
-});
