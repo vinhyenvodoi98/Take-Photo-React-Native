83ef33a378203a3f9ba7609706ea40f17dee1702
hoang <vinhyenvodoi98@gmail.com>
2019-08-07 23:38:45 +0700

    bug: livecycle does't work when click gallery ?

--- view/Home.js
+++ view/Home.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import * as FileSystem from 'expo-file-system';
 import { Camera } from 'expo-camera';
+
+import GalleryScreen from './GalleryScreen';
 
 export default class Home extends React.Component {
   state = {
     hasCameraPermission: null,
-    type: Camera.Constants.Type.back
+    type: Camera.Constants.Type.back,
+    newPhotos: false,
+    showGallery: false
   };
 
-  async componentDidMount() {
+  async componentWillMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
   }
+
+  componentDidMount() {
+    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch((e) => {
+      console.log(e, 'Directory exists');
+    });
+  }
+
+  takePicture = () => {
+    if (this.camera) {
+      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+    }
+  };
+
+  onPictureSaved = async (photo) => {
+    await FileSystem.moveAsync({
+      from: photo.uri,
+      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`
+    });
+    this.setState({ newPhotos: true });
+  };
+
+  toggleView = () => this.setState({ showGallery: !this.state.showGallery, newPhotos: false });
 
   render() {
     const { hasCameraPermission } = this.state;
@@ -20,32 +47,71 @@ export default class Home extends React.Component {
       return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
-    } else {
+    } else if (this.state.showGallery === true) {
+      return <GalleryScreen onPress={this.toggleView.bind(this)} />;
+    } else if (this.state.showGallery === false) {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+          <Camera
+            ref={(ref) => {
+              this.camera = ref;
+            }}
+            style={{ flex: 1 }}
+            type={this.state.type}>
             <View
               style={{
                 flex: 1,
                 backgroundColor: 'transparent',
-                flexDirection: 'row'
+                flexDirection: 'column'
               }}>
-              <TouchableOpacity
+              <View style={{ flex: 5 }} />
+
+              <View
                 style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center'
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back
-                  });
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'space-evenly'
+                  // alignSelf: 'flex-end'
                 }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-              </TouchableOpacity>
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      flex: 1
+                    }}
+                    onPress={() => {
+                      this.setState({
+                        type:
+                          this.state.type === Camera.Constants.Type.back
+                            ? Camera.Constants.Type.front
+                            : Camera.Constants.Type.back
+                      });
+                    }}>
+                    <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> FLIP </Text>
+                  </TouchableOpacity>
+                </View>
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      flex: 1
+                    }}
+                    onPress={() => {
+                      this.takePicture();
+                    }}>
+                    <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> SNAP </Text>
+                  </TouchableOpacity>
+                </View>
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      flex: 1
+                    }}
+                    onPress={() => {
+                      this.toggleView.bind(this);
+                    }}>
+                    <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>GALLERY</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
           </Camera>
         </View>
